d2362fc61dbe63e601e270c882faa0e56eb201ac
Andreas Zimmerer <andreas.zimmerer@tum.de>
2017-08-11 22:25:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This software is licensed under GNU GPL v3.0  
+See LICENSE.txt for further information.
---
+++ src/main/java/com/jibbow/fastis/Main.java
@@ -0,0 +1,21 @@
+package com.jibbow.fastis;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Jibbow on 8/11/17.
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
+
+
