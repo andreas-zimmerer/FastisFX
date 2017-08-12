b886f8ae9b6f99930af9889957ebc51d1775168e
Andreas Zimmerer <andreas.zimmerer@tum.de>
2017-08-12 13:46:35 +0200

    refactored DayPane

--- src/main/java/com/jibbow/fastis/util/DayPane.java
+++ src/main/java/com/jibbow/fastis/util/DayPane.java
@@ -15,17 +15,26 @@ import java.util.Map;
  * Created by Jibbow on 8/11/17.
  */
 public class DayPane extends PercentPane {
-    LocalDate dayDate;
-    ObjectProperty<LocalTime> dayStartTime;
-    ObjectProperty<LocalTime> dayEndTime;
-    Map<Appointment, Pane> appointments;
-    DayPaneRenderer renderer = new DayPaneRenderer();
+    private LocalDate dayDate;
+    private ObjectProperty<LocalTime> dayStartTime;
+    private ObjectProperty<LocalTime> dayEndTime;
+    private Map<Appointment, Pane> appointments;
+    private DayPaneRenderer renderer;
 
+
+    public DayPane(LocalDate date) {
+        this(date, LocalTime.MIN, LocalTime.MAX);
+    }
 
     public DayPane(LocalDate date, LocalTime dayStartTime, LocalTime dayEndTime) {
+        this(date, dayStartTime, dayEndTime, new DayPaneRenderer());
+    }
+
+    public DayPane(LocalDate date, LocalTime dayStartTime, LocalTime dayEndTime, DayPaneRenderer renderer) {
         this.dayDate = date;
         this.dayStartTime = new SimpleObjectProperty<>(dayStartTime);
         this.dayEndTime = new SimpleObjectProperty<>(dayEndTime);
+        this.renderer = renderer;
 
         this.appointments = new HashMap<>();
 
@@ -39,21 +48,55 @@ public class DayPane extends PercentPane {
         });
     }
 
+
+    /**
+     * Adds a new appointment to the DayPane.
+     * Calculates the correct position and layout and changes them if the appointment's interval
+     * is updated.
+     *
+     * @param appointment The appointment that should be added to the DayPane.
+     */
     public void addAppointment(Appointment appointment) {
-        appointments.put(appointment, addGuiElement(appointment));
+        Pane p = addGuiElement(appointment);
+        appointments.put(appointment, p);
+        renderer.layoutAppointments(appointments);
+
         appointment.intervalProperty().addListener(observable -> {
             appointments.put(appointment, addGuiElement(appointment));
             renderer.layoutAppointments(appointments);
         });
     }
 
-    private Pane addGuiElement(Appointment appointment) {
-        Pane pane = null;
+
+    /**
+     * Creates a gui element for an appointment with an associated {@link DayPaneRenderer}.
+     * The gui element is automatically added to (or removed from) the DayPane.
+     * According to the displayed time interval of the DayPane the relative vertical position
+     * of the gui element is set. If the appointments is outside the displayed time interval no
+     * gui element will be created or - if there has already been a gui element but the
+     * appointment's time or the DayPanes time constraints has been changed - destroyed and
+     * removed from the DayPane if this method is invoked after the change.
+     *
+     * @param appointment The appointment to which a gui elemnt will be created and added to the
+     *                    DayPane.
+     *
+     * @return  Returns null if no element has been created or if the element has been destroyed,
+     *          both because the appointments datetime is outside of the time constraints of the
+     *          DayPane. Otherwise the new gui element is returned.
+     */
+    protected Pane addGuiElement(Appointment appointment) {
+        // check if there is an existing gui element
+        Pane pane = appointments.getOrDefault(appointment, null);
+
+        // check if the appointment should be displayed
         if(appointment.intervalProperty().get().overlaps(
                 new TimeInterval(
                         LocalDateTime.of(dayDate, dayStartTime.get()),
                         LocalDateTime.of(dayDate, dayEndTime.get())))) {
-            pane = renderer.createGuiElement(appointment);
+
+            if(pane == null) {
+                pane = renderer.createGuiElement(appointment);
+            }
 
             // calculate minutes per day displayed; used for calculating the percentage
             long minutesPerDay = Duration.between(dayStartTime.get(), dayEndTime.get()).toMinutes();
@@ -74,7 +117,13 @@ public class DayPane extends PercentPane {
             }
 
             this.getChildren().add(pane);
+            return pane;
+        } else {
+            if(pane != null) {
+                // there is an existing gui element although is is not displayed -> remove it
+                this.getChildren().remove(pane);
+            }
+            return null;
         }
-        return pane;
     }
 }
