1ab5d9a93e796fdb6db49c04f33e3c20e6d0ec8e
Andreas Zimmerer <andreas.zimmerer@tum.de>
2017-08-14 20:57:20 +0200

    integrated DayChooser into DayView

--- src/main/java/com/jibbow/fastis/rendering/DayViewRenderer.java
+++ src/main/java/com/jibbow/fastis/rendering/DayViewRenderer.java
@@ -1,11 +1,14 @@
 package com.jibbow.fastis.rendering;
 
 import com.jibbow.fastis.Appointment;
+import com.jibbow.fastis.DayChooser;
 import com.jibbow.fastis.DayView;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 
@@ -26,26 +29,36 @@ public class DayViewRenderer extends DayPaneRenderer {
     }
 
     public Node createHeaderPane(DayView calView) {
-        GridPane container = new GridPane();
+        final GridPane container = new GridPane();
         container.setAlignment(Pos.BOTTOM_LEFT);
         container.getStyleClass().add("headerpane");
 
-        Label lblWeekday = new Label(calView.getDate().get().format(DateTimeFormatter.ofPattern("EEE")));
+        final Label lblWeekday = new Label(calView.getDate().get().format(DateTimeFormatter.ofPattern("EEE")));
         lblWeekday.getStyleClass().add("label-weekday");
 
-        Label lblDate = new Label(calView.getDate().get().toString());
+        final Label lblDate = new Label(calView.getDate().get().toString());
         lblDate.getStyleClass().add("label-date");
+        final ContextMenu dayChooserMenu = new ContextMenu();
+        final CustomMenuItem item = new CustomMenuItem(new DayChooser(calView.getDate()));
+        dayChooserMenu.getStyleClass().add("day-chooser");
+        item.setHideOnClick(false);
+        dayChooserMenu.getItems().add(item);
+        lblDate.setOnMouseClicked(event ->
+                dayChooserMenu.show(lblDate,
+                        lblDate.localToScreen(0, 0).getX(),
+                        lblDate.localToScreen(0, 0).getY())
+        );
 
-        Button left = new Button("<");
+        final Button left = new Button("<");
         left.getStyleClass().add("day-header-button");
         left.setOnAction(event -> calView.getDate().set(calView.getDate().get().minusDays(1)));
-        Button right = new Button(">");
+        final Button right = new Button(">");
         right.getStyleClass().add("day-header-button");
         right.setOnAction(event -> calView.getDate().set(calView.getDate().get().plusDays(1)));
 
-        ColumnConstraints columnWeekday = new ColumnConstraints(70);
-        ColumnConstraints columnCenter = new ColumnConstraints(20,50,Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.LEFT,true);
-        ColumnConstraints columnSwitcher = new ColumnConstraints(60);
+        final ColumnConstraints columnWeekday = new ColumnConstraints(70);
+        final ColumnConstraints columnCenter = new ColumnConstraints(20,50,Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.LEFT,true);
+        final ColumnConstraints columnSwitcher = new ColumnConstraints(60);
         container.getColumnConstraints().addAll(columnWeekday, columnCenter, columnSwitcher);
         container.add(lblWeekday,0,0);
         container.add(lblDate, 1,0);
